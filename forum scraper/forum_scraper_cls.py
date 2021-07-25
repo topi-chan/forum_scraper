d5235c7c086412bdfa84a648177e16bf4cd465f6
Maciej Topolewski <maciej@top-project.com.pl>
2021-07-25 16:34:57 +0200

    scraper forum 1.2

--- forum scraper/forum_scraper_cls.py
+++ forum scraper/forum_scraper_cls.py
@@ -21,25 +21,42 @@ class ScraperFromLink():
             'Accept': 'test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Referer': 'http://www.google.com/'}
 
-    def get_link(self, type, name, main_link, lookup):
-        www = urllib.request.urlopen(self.url, context=self.ctx).read()
+    def list_to_string(self, bs4_content):
+        str1 = " "
+        return (str1.join(bs4_content))
 
-def iterate_scraper(lenght, first_url, lookup):
+    def get_link(self, type, name, lookup):
+        www = urllib.request.urlopen(self.url, context=self.ctx).read()
+        soup = BeautifulSoup(www, 'html.parser')
+        div = soup.find_all(type, name)
+        for elem in div:
+            self.paragraphs.append(str(elem))
+        soup = BeautifulSoup((self.list_to_string(self.paragraphs)), 'html.parser')
+        tags = soup('a')
+        for tag in tags:
+            self.tag_lib.append(tag.get('href', None))
+        for tag in self.tag_lib:
+            if lookup in tag:
+                self.link_lib.append(lookup)
+
+
+
+
+
+def iterate_scraper(lenght, first_url, lookup, site, name):
     content = []
     links = []
     for i in range(lenght):
         obj1 = ScraperFromLink(first_url)
-        obj1.get_link("div", {"class": "topictitle"},
-            "https://www.ncbr.gov.pl", lookup)
+        obj1.get_link("div", {"class": "topictitle"})
         obj1.get_content_ncbr_link()
         content.extend(obj1.content)
         links.extend(obj1.link_lib)
         obj2 = ScraperFromLink(first_url)
-        obj2.get_link("li", {"class": "last next"},"https://www.ncbr.gov.pl",
-        "news")
+        obj2.get_link("li", {"class": "last next"}, site, name)
         first_url = obj2.link_lib[0]
         print("x")
     return content, links
 
 if __name__ == '__main__':
-    iterate_scraper(lenght, first_url, lookup)
+    iterate_scraper()
---
+++ forum scraper/fr_scrp_implement.py
@@ -0,0 +1,1 @@
+from forum_scraper_cls import *
